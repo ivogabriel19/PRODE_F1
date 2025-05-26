14192c39b7cb17243a0781b567ae7ff728bccfa5
ivo-gabriel <ivo.degange@ort.edu.ar>
2025-05-26 09:54:06 -0300

    funciona boton admin carga usuarios

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -5,7 +5,7 @@ async function procesar() {
   });
   const data = await res.json();
   document.getElementById('mensaje').innerText = data.mensaje || 'Error';
-  cargarUsuarios();
+  //cargarUsuarios();
 }
 
 async function cargarUsuarios() {
@@ -15,6 +15,8 @@ async function cargarUsuarios() {
   const data = await res.json();
   const ul = document.getElementById('usuarios');
   ul.innerHTML = '';
+
+  console.log(data);
 
   data.forEach(u => {
     ul.innerHTML += `<li>${u.username || u.email} – ${u.totalScore} puntos – Rol: ${u.role}</li>`;
